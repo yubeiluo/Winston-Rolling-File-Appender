db2d8f2b2956b70156985df158e3c189ad8f9942
david <david@0qian.com>
2015-06-28 18:53:47 +0800

    my initial version.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-Winston-Rolling-File-Appender
+Another-Winston-Rolling-File-Appender
 =============================
 
+This repo is forked from the repo [Winston-Rolling-File-Appender](https://github.com/mallocator/Winston-Rolling-File-Appender) per the below Rationale section.
+
+# Rationale
+
+1. Per the issue [Losing log messages](https://github.com/winstonjs/winston/issues/567) and my personal experience, when using the official **DailyFileTransport**, some messages will get lost and fail to appear in the log file.
+
+2. After trying out what is described in  [Winston-Rolling-File-Appender](https://github.com/mallocator/Winston-Rolling-File-Appender), the issue still persists, so I decided to hand craft my own version.
+
+# Introduction
+
 A rolling file transport for the logging library winston for node.js.
-This transport has been modified from the original transport to create a log file for each day.
+This transport has been modified from [Winston-Rolling-File-Appender](https://github.com/mallocator/Winston-Rolling-File-Appender)  to create a log file for each day.
 
 If configured with my.log as filename, the generated files will be for example:
 
@@ -13,37 +23,29 @@ If configured with my.log as filename, the generated files will be for example:
 	my.2012-08-10.log
 	my.log ( -> symbolic link to latest log file)
 
-The transport has been used and tested on linux machines. No idea if this works on windows.
-
+The transport has been used and tested on Linux and Mac machines. No idea if this works on windows.
 
 # Usage
 
 ```javascript
 var winston = require('winston');
-require('rolling-file-transport');
+	, RollingFile = require('another-rolling-file-transport');
 
-winston.loggers.add('myLogger', {
-	rollingFile : {
-		filename : '/path/to/my/filename.log',	// files will use filename.<date>.log for all files 
-		level : 'info',							// Set your winston log level, same as original file transport
-		timestamp : true,						// Set timestmap format/enabled, Same ass original file transport
-		maxFiles : 10,							// How many days to keep as back log
-		json : false							// Store logging data ins json format
-	}
-});
+var logger = new winston.Logger();
+
+var options = {
+	filename : '/path/to/my/filename.log',	// files will use filename.<date>.log for all files
+	level : 'info',							// Set your winston log level, same as original file transport
+	timestamp : true,						// Set timestmap format/enabled, Same ass original file transport
+	maxFiles : 10,							// How many days to keep as back log
+	json : false							// Store logging data ins json format
+};
+
+logger.add(RollingFile, options);
 ```
 
 # Install
-for [node.js](http://nodejs.org/) and [npm](https://npmjs.org), use the package.json to install your dependecies (running "npm install"):
 
-	{
-		...
-		"dependencies": {
-			"winstonRollingTransport" : "git://github.com/mallocator/Winston-Rolling-File-Appender.git#master"
-		}
-		...
-	}
+For [node.js](http://nodejs.org/) and [npm](https://npmjs.org), run the "npm install" to provision this package in your app.
 
-or install it manually
-
-	npm install git://github.com/mallocator/Winston-Rolling-File-Appender.git
+	npm install git@github.com:yubeiluo/Winston-Rolling-File-Appender.git --save
--- rollingFile.js
+++ rollingFile.js
@@ -1,75 +1,87 @@
-var winston = require('winston');
-var fs = require('fs');
-var path = require('path');
-var util = require('util');
-var Transport = winston.Transport;
+/**
+ * Rolling file transport forked from https://github.com/mallocator/Winston-Rolling-File-Appender.
+ *
+ * The built in DailyFileTransport have message loss issues per described in https://github.com/winstonjs/winston/issues/567,
+ * therefore, we need to hand-craft our own version of rolling transport.
+ *
+ * Created by david on 6/28/15.
+ * @author David Yu <davidyu@squarevalleytech.com>
+ */
+
+var winston = require('winston')
+  , fs = require('fs')
+  , path = require('path')
+  , util = require('util')
+  , Transport = winston.Transport;
 
 function log(options) {
-	var timestamp = typeof options.timestamp === 'function' ? options.timestamp() : options.timestamp ? new Date().toString().substr(4, 20) : null;
-	var output;
-	
-	if (options.json) {
-		output = {};
-		output.level = options.level;
-		output.message = options.message;
-		
-		if (timestamp) {
-			output.timestamp = timestamp;
-		}
-		
-		return JSON.stringify(output, function(key, value) {
-			if (value instanceof Buffer) { return value.toString('base64'); }
-			return value;
-		});
-	}
-	
-	output = timestamp ? timestamp + ' - ' : '';
-	output += options.colorize ? config.colorize(options.level) : options.level;
-	output += ': ' + options.message;
-	
-	return output;
+  var timestamp = typeof options.timestamp === 'function' ? options.timestamp() : options.timestamp ? new Date().toString().substr(4, 20) : null;
+  var output;
+
+  if (options.json) {
+    output = {};
+    output.level = options.level;
+    output.message = options.message;
+
+    if (timestamp) {
+      output.timestamp = timestamp;
+    }
+
+    return JSON.stringify(output, function(key, value) {
+      if (value instanceof Buffer) { return value.toString('base64'); }
+      return value;
+    });
+  }
+
+  output = timestamp ? timestamp + ' - ' : '';
+  output += options.colorize ? config.colorize(options.level) : options.level;
+  output += ': ' + options.message;
+
+  return output;
 };
 
 var RollingFile = winston.transports.RollingFile = exports.RollingFile = function(options) {
-	Transport.call(this, options);
-	
-	function throwIf(target) {
-		Array.prototype.slice.call(arguments, 1).forEach(function(name) {
-			if (options[name]) { throw new Error('Cannot set ' + name + ' and ' + target + 'together'); }
-		});
-	}
-	
-	if (options.filename || options.dirname) {
-		throwIf('filename or dirname', 'stream');
-		this._filename = this.filename = options.filename ? path.basename(options.filename) : 'winston.log';
-		this.dirname = options.dirname ? options.dirname : path.dirname(options.filename);
-		this.options = options.options ? options.options : {
-			flags : 'a'
-		};
-	} else {
-		throw new Error('Cannot log to file without filename or stream.');
-	}
-	if (options.checkPermissions === null)
-        options.checkPermissions = true;
-	
-	if (options.checkPermissions) {
-		function canWrite(owner, inGroup, mode) {
-			return owner && mode & 00200 || inGroup && mode & 00020 || mode & 00002;
-		}
-		var stat = fs.statSync(this.dirname);
-		if (!canWrite(process.getuid() === stat.uid, process.getgid() === stat.gid, stat.mode)) { 
-			throw new Error('Cannot create logs in directory "' + this.dirname + '"'); 
-		}
-	}
-	this.json = options.json !== false;
-	this.colorize = options.colorize || false;
-	this.maxFiles = options.maxFiles ? options.maxFiles : 10;
-	this.timestamp = typeof options.timestamp !== 'undefined' ? options.timestamp : false;
-	this._buffer = [];
-	
-	this._ext = path.extname(this._filename);
-	this._basename = path.basename(this._filename, this._ext);
-	this._oldFilesRegEx = new RegExp(this._basename + '\\.[0-9\\-]*\\.' + this._ext.substr(1));
+  Transport.call(this, options);
+
+  function throwIf(target) {
+    Array.prototype.slice.call(arguments, 1).forEach(function(name) {
+      if (options[name]) { throw new Error('Cannot set ' + name + ' and ' + target + 'together'); }
+    });
+  }
+
+  if (options.filename || options.dirname) {
+    throwIf('filename or dirname', 'stream');
+    this._filename = this.filename = options.filename ? path.basename(options.filename) : 'winston.log';
+    this.dirname = options.dirname ? options.dirname : path.dirname(options.filename);
+    this.options = options.options ? options.options : {
+      flags : 'a'
+    };
+  } else {
+    throw new Error('Cannot log to file without filename or stream.');
+  }
+  if (options.checkPermissions === null)
+    options.checkPermissions = true;
+
+  if (options.checkPermissions) {
+    function canWrite(owner, inGroup, mode) {
+      return owner && mode & 00200 || inGroup && mode & 00020 || mode & 00002;
+    }
+    var stat = fs.statSync(this.dirname);
+    if (!canWrite(process.getuid() === stat.uid, process.getgid() === stat.gid, stat.mode)) {
+      throw new Error('Cannot create logs in directory "' + this.dirname + '"');
+    }
+  }
+  this.json = options.json !== false;
+  // this flag marks wthether a symbolic link is desired or not, defaul is true
+  this.symbolicLink = options.symboliclink !== false;
+  this.colorize = options.colorize || false;
+  this.maxFiles = options.maxFiles ? options.maxFiles : 10;
+  this.timestamp = typeof options.timestamp !== 'undefined' ? options.timestamp : false;
+  this._buffer = [];
+
+  this._ext = path.extname(this._filename);
+  this._basename = path.basename(this._filename, this._ext);
+  this._oldFilesRegEx = new RegExp(this._basename + '\\.[0-9\\-]*\\.' + this._ext.substr(1));
 };
 
 // Inherit from `winston.Transport`.
@@ -85,162 +97,109 @@ RollingFile.prototype.name = 'rollingFile';
 // #### @callback {function} Continuation to respond to when complete.
 // Core logging method exposed to Winston. Metadata is optional.
 RollingFile.prototype.log = function(level, msg, meta, callback) {
-	if (this.silent) { return callback(null, true); }
-	
-	var output = log({
-		level : level,
-		message : msg,
-		meta : meta,
-		json : this.json,
-		colorize : this.colorize,
-		timestamp : this.timestamp
-	}) + '\n';
-	
-	var self = this;
-	if (!this.open()) {
-		return self._buffer.push(output);
-	} else {
-		self.stream.write(output);
-		self._lazyDrain();
-	}
-	
-	callback(null, true);
+  if (this.silent) { return callback(null, true); }
+
+  var output = log({
+      level : level,
+      message : msg,
+      meta : meta,
+      json : this.json,
+      colorize : this.colorize,
+      timestamp : this.timestamp
+    }) + '\n';
+
+  this._write(output);
+  callback(null, true);
 };
 
-// ### function open (callback)
-// #### @callback {function} Continuation to respond to when complete
-// Checks to see if a new file needs to be created based on the `maxsize` (if any) and the current size of the file used.
-RollingFile.prototype.open = function() {
-	if (this.opening) { return false; }
-	var todayString = new Date().toISOString().substr(0, 10);
-	if (!this.streamDate || this.StreamDate != todayString) {
-		this.opening = true;
-		this._createStream(todayString);
-		return false;
-	}
-	return true;
+/**
+ * The main function to write the message.
+ */
+RollingFile.prototype._write = function(output) {
+  var currentLogFilePath = this._currentLogFilePath
+    , newLogFilePath = path.join(this.dirname, this.getCurrentLogFileName())
+    , logFilePathChanged = false;
+
+  if ( !currentLogFilePath || newLogFilePath !== currentLogFilePath ) {
+    this._currentLogFilePath = newLogFilePath;
+    logFilePathChanged = true;
+  }
+
+  fs.appendFileSync(this._currentLogFilePath, output);
+  if ( logFilePathChanged ) {
+    this._createLink(this._currentLogFilePath);
+    this._cleanOldFiles();
+  }
+
 };
 
-// ### function close ()
-// Closes the stream associated with this instance.
-RollingFile.prototype.close = function() {
-	var self = this;
-	
-	if (this.stream) {
-		this.stream.end();
-		this.stream.destroySoon();
-		
-		this.stream.once('drain', function() {
-			self.emit('flush');
-			self.emit('closed');
-		});
-	}
+/**
+ * Create the symbolic link if desired.
+ * @params {String} target - the target file to be symbolic linked against.
+ * @private
+ */
+RollingFile.prototype._createLink = function(target) {
+  if ( !this.symbolicLink ) return;
+
+  var linkFilePath = path.join(this.dirname, this._basename + this._ext)
+    , linkName = path.basename(linkFilePath)
+    , targetName = path.basename(target)
+    , exists = fs.existsSync(linkFilePath)
+    , create = true;
+
+  if ( exists ) {
+    var oldTargetName = fs.readlinkSync(linkName);
+    if ( oldTargetName != targetName ) {
+      console.log('removing old link %s => %s', linkName, oldTargetName);
+      fs.unlinkSync(linkName);
+    } else {
+      create = false;
+    }
+  }
+
+  if ( create ) {
+    console.log('creating link %s => %s', linkName, targetName);
+    fs.symlinkSync(targetName, linkFilePath);
+  }
 };
 
-// ### function flush ()
-// Flushes any buffered messages to the current `stream` used by this instance.
-RollingFile.prototype.flush = function() {
-	var self = this;
-	// Iterate over the `_buffer` of enqueued messaged and then write them to the newly created stream.
-	this._buffer.forEach(function(str) {
-		process.nextTick(function() {
-			self.stream.write(str);
-			self._size += str.length;
-		});
-	});
-	// Quickly truncate the `_buffer` once the write operations have been started
-	self._buffer.length = 0;
-	// When the stream has drained we have flushed our buffer.
-	self.stream.once('drain', function() {
-		self.emit('flush');
-		self.emit('logged');
-	});
+/**
+ * Get the current log file name
+ * @returns {String}
+ */
+RollingFile.prototype.getCurrentLogFileName = function() {
+  var todayString = new Date().toISOString().substr(0, 10)
+
+  return util.format("%s.%s%s", this._basename, todayString, this._ext);
 };
 
-// ### @private function _createStream ()
-// Attempts to open the next appropriate file for this instance based on the common state (such as `maxsize` and
-// `_basename`).
-RollingFile.prototype._createStream = function(dateString) {
-	if (this.stream) {
-		this.flush();
-	}
-	
-	var self = this;
-	var filename = path.join(this.dirname, this._basename + '.' + dateString + this._ext);
-	fs.exists(filename, function(exists) {
-		self.stream = fs.createWriteStream(filename, {
-			'flags' : exists ? 'a' : 'w'
-		});
-		self.once('flush', function() {
-			self.opening = false;
-			self.emit('open', filename);
-		});
-		self.flush();
-	});
-	this._createLink(filename);
-	this._cleanOldFiles();
-};
-
-RollingFile.prototype._createLink = function(filename) {
-	var linkName = path.join(this.dirname, this._basename + this._ext);
-    filename = path.basename(filename);
-	function link() {
-		fs.symlink(filename, linkName);
-	}
-	
-	fs.exists(linkName, function(exists) {
-		if (exists) {
-			fs.readlink(linkName, function(err, dst) {
-				if (dst != filename) {
-					fs.unlink(linkName, link);
-                console.log('unlinked: ' +dst + " " + filename)
-				}
-			});
-		} else {
-			link();
-		}
-	});
-};
-
+/**
+ * Only keep {maxFiles} in the log directory by cleaning the old files
+ * @private
+ */
 RollingFile.prototype._cleanOldFiles = function() {
-	var whitelist = {};
-	var date = new Date();
-	for ( var i = 0; i < this.maxFiles; i++) {
-		var filename = this._basename + '.' + date.toISOString().substr(0, 10) + this._ext;
-		whitelist[filename] = true;
-		date.setDate(date.getDate() - 1);
-	}
-	var self = this;
-	fs.readdir(this.dirname, function(err, files) {
-		if (err) {
-			console.log('There has been an error while trying to clean old log files:', err);
-			return;
-		}
-		if (!files) {
-			console.log('No (log) files found, probably permissions problem on directory');
-			return;
-		}
-		files.forEach(function(file) {
-			if (self._oldFilesRegEx.test(file) && !whitelist[file]) {
-				fs.unlink(path.join(self.dirname, file));
-			}
-		});
-	});
-};
+  var whitelist = {}
+    , maxFiles = this.maxFiles
+    , date = new Date();
 
-// ### @private function _lazyDrain ()
-// Lazily attempts to emit the `logged` event when `this.stream` has drained. This is really just a simple mutex that only
-// works because Node.js is single-threaded.
-RollingFile.prototype._lazyDrain = function() {
-	var self = this;
-	
-	if (!this._draining && this.stream) {
-		this._draining = true;
-        
-        var that = this;		
-		this.stream.once('drain', function() {
-			that._draining = false;
-			self.emit('logged');
-		});
-	}
+  for ( var i = 0; i < maxFiles; i++) {
+    var filename = this._basename + '.' + date.toISOString().substr(0, 10) + this._ext;
+    whitelist[filename] = true;
+    date.setDate(date.getDate() - 1);
+  }
+
+  var files = fs.readdirSync(this.dirname);
+  if ( !files ) {
+    console.log('No (log) files found, probably permissions problem on directory');
+    return;
+  }
+
+  var self = this;
+  files.forEach(function(file) {
+    if (self._oldFilesRegEx.test(file) && !whitelist[file]) {
+      console.log('removing old file %s', file);
+      fs.unlinkSync(path.join(self.dirname, file));
+    }
+  });
+
 };
